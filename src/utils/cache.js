460412637e3cc13cba5894f6e9eb92bc3b09c9cc
Sateesh Ananatharamaiah <vn05523@delhaize.com>
2019-12-19 14:13:00 +0530

    adding redis cache

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -1,16 +1,16 @@
 
-function cache_set(key, value, callback) {
+function set(key, value, callback) {
     redisClient.set(JSON.stringify(key), value, function(error, result) {
         console.log("Cache : Storing for key - " + JSON.stringify(key));
         return callback(error, result);
     });
 }
 
-function cache_get(key, callback) {
+function get(key, callback) {
     redisClient.get(JSON.stringify(key), function(err, result){
         console.log("Cache : Got result for key - " + JSON.stringify(key) );
         return callback(err, result);
     });
 }
 
-module.exports = {cache_get, cache_set};
+module.exports = {get, set};
